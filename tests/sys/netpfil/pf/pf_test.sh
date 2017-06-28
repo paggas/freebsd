d04bd679885739dda3fd8fdadc1b3564660b3cf2
Panagiotes Mousikides <paggas@freebsd.org>
2017-06-28 22:03:22 +0000

    tests/sys/netpfil/pf: block_drop test

--- tests/sys/netpfil/pf/pf_test.sh
+++ tests/sys/netpfil/pf/pf_test.sh
@@ -32,6 +32,25 @@ block_return_cleanup () {
     [ -e nc.50001.pid ] && kill `cat nc.50001.pid`
 }
 
+atf_test_case block_drop cleanup
+block_drop_head () {
+    atf_set descr 'Block-with-drop a port and test that it is blocked.'
+}
+block_drop_body () {
+    rules="block drop in on $REMOTE_IF proto tcp to port 50000"
+    echo "$rules" | atf_check -e ignore ssh "$SSH" pfctl -ef -
+    atf_check daemon -p nc.50000.pid ssh "$SSH" nc -l 50000
+    atf_check daemon -p nc.50001.pid ssh "$SSH" nc -l 50001
+    atf_check -s exit:1 -e empty  nc -z -w 4 "$REMOTE_ADDR" 50000
+    atf_check -s exit:0 -e ignore nc -z "$REMOTE_ADDR" 50001
+}
+block_drop_cleanup () {
+    atf_check -e ignore ssh "$SSH" pfctl -dFa
+    [ -e nc.50000.pid ] && kill `cat nc.50000.pid`
+    [ -e nc.50001.pid ] && kill `cat nc.50001.pid`
+}
+
 atf_init_test_cases () {
     atf_add_test_case block_return
+    atf_add_test_case block_drop
 }
