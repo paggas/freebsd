b2d8f6507252868dab7e30cda1787fd8b5eac218
Panagiotes Mousikides <paggas@freebsd.org>
2017-06-28 21:21:50 +0000

    tests/sys/netpfil/pf: documentation

--- tests/sys/netpfil/pf/pf_test.sh
+++ tests/sys/netpfil/pf/pf_test.sh
@@ -9,30 +9,29 @@
 
 . "$(atf_get_srcdir)/pf_test.conf.sh"
 
-atf_test_case block cleanup
-block_head () {
-    atf_set descr \
-"Tests that a port on a remote test machine is properly blocked.  \
-Starts two instances of nc on the remote machine, listening on two \
-different ports.  One of the ports is blocked with return by the \
-remote pf.  Tries then to connect to the two instances from the \
-local machine.  The test succeeds if one connection succeeds but the \
-other one fails."
+# Starts two instances of nc on the remote machine, listening on two
+# different ports, of which one port is blocked-with-return by the
+# remote pf.  The test tries then to connect to the two instances from
+# the local machine.  The test succeeds if one connection succeeds but
+# the other one fails.
+atf_test_case block_return cleanup
+block_return_head () {
+    atf_set descr 'Block-with-return a port and test that it is blocked.'
 }
-block_body () {
-    echo "block return in on $REMOTE_IF proto tcp to port 50000" | \
-	atf_check -e ignore ssh "$SSH" pfctl -ef -
+block_return_body () {
+    rules="block return in on $REMOTE_IF proto tcp to port 50000"
+    echo "$rules" | atf_check -e ignore ssh "$SSH" pfctl -ef -
     atf_check daemon -p nc.50000.pid ssh "$SSH" nc -l 50000
     atf_check daemon -p nc.50001.pid ssh "$SSH" nc -l 50001
     atf_check -s exit:1 -e empty  nc -z "$REMOTE_ADDR" 50000
     atf_check -s exit:0 -e ignore nc -z "$REMOTE_ADDR" 50001
 }
-block_cleanup () {
-    atf_check -e ignore ssh "$SSH" "pfctl -dFa"
+block_return_cleanup () {
+    atf_check -e ignore ssh "$SSH" pfctl -dFa
     [ -e nc.50000.pid ] && kill `cat nc.50000.pid`
     [ -e nc.50001.pid ] && kill `cat nc.50001.pid`
 }
 
 atf_init_test_cases () {
-    atf_add_test_case block
+    atf_add_test_case block_return
 }
