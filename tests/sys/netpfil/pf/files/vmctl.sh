a00f8983edcba62dd4e5501891612621538ce028
Panagiotes Mousikides <paggas@freebsd.org>
2017-07-25 14:10:25 +0000

    tests/sys/netpfil/pf: cleaned up vmctl.sh

--- tests/sys/netpfil/pf/files/vmctl.sh
+++ tests/sys/netpfil/pf/files/vmctl.sh
@@ -28,74 +28,80 @@ mountdir="/mnt/tests/pf/vm.${vm}"
 make_baseimg () {
     [ -e "/dev/zvol/${baseimg}" ] && return
     tempdir="$(mktemp -d)"
-    imgfile="${tempdir}/FreeBSD-11.0-RELEASE-amd64.raw"
-    { fetch -o "${imgfile}.xz" \
-    	    "https://download.freebsd.org/ftp/releases/VM-IMAGES/11.0-RELEASE/amd64/Latest/FreeBSD-11.0-RELEASE-amd64.raw.xz" &&
-    	  unxz "${imgfile}.xz" ; } || return 1
-    size="$(stat -f '%z' ${imgfile})"
-    # Round up to multiple of 16M.
-    [ "$(expr ${size} % 16777216)" = 0 ] ||
-	size="$(expr \( \( $size / 16777216 \) + 1 \) \* 16777216)"
-    zfs create -p -V "${size}" "${baseimg}" &&
-	dd bs=16M if="${imgfile}" of="/dev/zvol/${baseimg}"
+    (
+        # Download image file.
+        imgfile="${tempdir}/FreeBSD-11.0-RELEASE-amd64.raw"
+        fetch -o "${imgfile}.xz" \
+              "https://download.freebsd.org/ftp/releases/VM-IMAGES/11.0-RELEASE/amd64/Latest/FreeBSD-11.0-RELEASE-amd64.raw.xz" \
+            || return 1
+        unxz "${imgfile}.xz" || return 1
+        size="$(stat -f '%z' ${imgfile})"
+        # Round up to multiple of 16M.
+        [ "$(expr ${size} % 16777216)" = 0 ] ||
+            size="$(expr \( \( $size / 16777216 \) + 1 \) \* 16777216)"
+        # Copy image file to zvol.
+        zfs create -p -V "${size}" "${baseimg}" || return 1
+        dd bs=16M if="${imgfile}" of="/dev/zvol/${baseimg}" || return 1
+    )
     status="$?"
-    rm -r "${tempdir}" && return "${status}"
+    rm -r "${tempdir}"
+    return "${status}"
 }
 
 write_sshlogin () {
     addr="$(grep -E "ifconfig_.*inet.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" \
                 "vmctl.${vm}.rcappend" |
             sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*/\1/" |
-            head -n 1)" &&
-	[ "x${addr}" '!=' "x" ] &&
-	echo "root@${addr}" > "vmctl.${vm}.sshlogin"
+            head -n 1)" || return 1
+    [ "x${addr}" '!=' "x" ] || return 1
+    echo "root@${addr}" > "vmctl.${vm}.sshlogin" || return 1
 }
 
 case "${cmd}" in
     (create)
-	# make -f "${PF_TEST_DIR}/files/vmctl.mk" \
-	#      "/dev/zvol/${zdir}/baseimg"
-	{
-	    make_baseimg &&
-		zfs snap "${snap}" &&
-		zfs clone "${snap}" "${vmimg}" &&
-		ssh-keygen -q -P '' -f "vmctl.${vm}.id_rsa" &&
-		write_sshlogin &&
-		mkdir -p "${mountdir}" &&
-		mount "/dev/zvol/${vmimg}p3" "${mountdir}"
-	} || exit 1
-	mkdir -p "${mountdir}/root/.ssh" &&
-	    cat "vmctl.${vm}.id_rsa" >> \
-		"${mountdir}/root/.ssh/authorized_keys" &&
-	    echo "PermitRootLogin without-password" >> \
-		 "${mountdir}/etc/ssh/sshd_config" &&
-	    echo "sshd_enable=\"YES\"" >> \
-		 "${mountdir}/etc/rc.conf" &&
-	    cat "vmctl.${vm}.rcappend" >> \
-		"${mountdir}/etc/rc.conf"
-	appendstatus="$?"
-	umount "${mountdir}" &&
-	    rmdir "${mountdir}" &&
-	    [ "x${appendstatus}" = 'x0' ] && {
-		ifsopt=''
-		for i in ${ifs} ; do
-		    ifsopt="${ifsopt} -t ${i}" ; done
-		daemon -p "vmctl.${vm}.pid" \
-		       sh /usr/share/examples/bhyve/vmrun.sh ${ifsopt} \
-		       -d "/dev/zvol/${vmimg}" -C "${console}" \
-		       "tests-pf-${vm}"
-	    }
-	;;
+        make_baseimg || exit 1
+        zfs snap "${snap}" || exit 1
+        zfs clone "${snap}" "${vmimg}" || exit 1
+        ssh-keygen -q -P '' -f "vmctl.${vm}.id_rsa" || exit 1
+        write_sshlogin || exit 1
+        mkdir -p "${mountdir}" || exit 1
+        mount "/dev/zvol/${vmimg}p3" "${mountdir}" || exit 1
+        (
+            make_install || return 1
+            mkdir -p "${mountdir}/root/.ssh" || return 1
+            cat "vmctl.${vm}.id_rsa" >> \
+                "${mountdir}/root/.ssh/authorized_keys" || return 1
+            echo "PermitRootLogin without-password" >> \
+                 "${mountdir}/etc/ssh/sshd_config" || return 1
+            echo "sshd_enable=\"YES\"" >> \
+                 "${mountdir}/etc/rc.conf" || return 1
+            cat "vmctl.${vm}.rcappend" >> \
+                "${mountdir}/etc/rc.conf" || return 1
+        )
+        appendstatus="$?"
+        umount "${mountdir}"
+        rmdir "${mountdir}"
+        [ "x${appendstatus}" = 'x0' ] || return 1
+        (
+            ifsopt=''
+            for i in ${ifs} ; do
+                ifsopt="${ifsopt} -t ${i}" ; done
+            daemon -p "vmctl.${vm}.pid" \
+                   sh /usr/share/examples/bhyve/vmrun.sh ${ifsopt} \
+                   -d "/dev/zvol/${vmimg}" -C "${console}" \
+                   "tests-pf-${vm}"
+        )
+        ;;
     (destroy)
-	bhyvectl --destroy --vm="tests-pf-${vm}"
-	[ -e "vmctl.${vm}.pid" ] && kill "$(cat vmctl.${vm}.pid)"
-	rm "vmctl.${vm}.id_rsa" \
-	   "vmctl.${vm}.id_rsa.pub" \
-	   "vmctl.${vm}.sshlogin"
-	zfs destroy -R "${snap}"
-	;;
+        bhyvectl --destroy --vm="tests-pf-${vm}"
+        [ -e "vmctl.${vm}.pid" ] && kill "$(cat vmctl.${vm}.pid)"
+        rm "vmctl.${vm}.id_rsa" \
+           "vmctl.${vm}.id_rsa.pub" \
+           "vmctl.${vm}.sshlogin"
+        zfs destroy -R "${snap}"
+        ;;
     (*)
-	echo "${usage}" >&2
-	exit 1
-	;;
+        echo "${usage}" >&2
+        exit 1
+        ;;
 esac
