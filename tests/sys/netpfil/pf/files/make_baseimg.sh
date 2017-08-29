a40965bc079c174443958d2fab69cd7229669589
Panagiotes Mousikides <paggas@freebsd.org>
2017-08-29 11:00:20 +0000

    tests/sys/netpfil/pf/files/make_baseimg.sh

--- tests/sys/netpfil/pf/files/make_baseimg.sh
+++ tests/sys/netpfil/pf/files/make_baseimg.sh
@@ -18,7 +18,6 @@ sourcedir="${1}"
 
 ncpu="$(sysctl -n hw.ncpu)"
 baseimg="zroot/tests/pf/baseimg"
-zmountbase="$(zfs get -H -o value mountpoint "${baseimg}")" || exit 1
 mountdir="/mnt/tests/pf/baseimg"
 
 cd "${sourcedir}" || exit 1
@@ -28,16 +27,22 @@ make -j "${ncpu}" buildkernel || exit 1
 cd release || exit 1
 # TODO Instead of make clean, use an alternative target directory.
 make clean || exit 1
+
+sourcedir_canon="$(readlink -f ${sourcedir})"
+
 rm -r "/usr/obj${sourcedir_canon}/release" # force rebuilding by make release
 make release || exit 1
 make vm-image \
      WITH_VMIMAGES="1" VMBASE="vm-tests-pf" \
      VMFORMATS="raw" VMSIZE="3G" || exit 1
-sourcedir_canon="$(readlink -f ${sourcedir})"
 
 cd "/usr/obj${sourcedir_canon}/release" || exit 1
 zfs create -p "${baseimg}" || exit 1
-cp -ai vm-tests-pf.raw "${zmountbase}/img" || exit 1
+
+zmountbase="$(zfs get -H -o value mountpoint "${baseimg}")" || exit 1
+
+install -o root -g wheel -m 0644 \
+        "vm-tests-pf.raw" "${zmountbase}/img" || exit 1
 
 mkdir -p "${mountdir}" || exit 1
 md="$(mdconfig ${zmountbase}/img)" || exit 1
